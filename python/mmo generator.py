a8532efd4f7b5171229f117ed87bc66ddccb2be0
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2022-05-10 11:40:04 -0600

    add backup file, and log file in data folder (auto generated)

--- python/mmo generator.py
+++ python/mmo generator.py
@@ -1,5 +1,6 @@
+import os
 import mmo
-from datetime import date
+from datetime import datetime as date
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import *
@@ -92,6 +93,8 @@ def addTime():
     else:
         mmo.addTime(category,user,time)
 
+    backup()
+
 def delTime():
     print('\nenter nothing anytime to cancel')
     category = getCatName()
@@ -115,6 +118,7 @@ def delTime():
 
     if input('Are you sure you want to delete @'+user+"'s run " + str(mmo.leaderboard[category][user]) + ' (y/n):\n') == 'y' or 'yes':
         mmo.delTime(category,user)
+        backup()
 
 def viewLeaderboard():
     print('\nenter nothing anytime to cancel')
@@ -142,9 +146,14 @@ def export():
     path = filedialog.asksaveasfilename(initialfile=filename, title = "Save post as",filetypes = (("text files","*.txt"),("all files","*.*")))
     print(path)
 
-    f = open(path, 'w', encoding='utf8')
+    f = open(path, 'w+', encoding='utf8')
     f.write(txt)
     f.close()
+    
+    log('export successful')
+
+# def createError():
+#     raise 'this is an error'
 
 def runAction(action):
 
@@ -162,6 +171,42 @@ def runAction(action):
     action_fun = actions.get(action, invalid)
     return action_fun()
 
+def log(log):
+    root = os.getcwd()
+    try:
+        os.mkdir(root + '/data')
+    except:
+        pass
+
+    data = root + '/data'
+    
+    now = date.now()
+
+    epath = data + '/log.txt'
+    ef = open(epath, 'a', encoding='utf8')
+    line = [now.strftime("%H:%M:%S %m-%d-%y: ") + str(log) + '\n']
+    ef.writelines(line)
+    ef.close()
+
+def backup():
+    root = os.getcwd()
+
+    try:
+        os.mkdir(root + '/data')
+    except:
+        pass
+
+    data = root + '/data'
+
+    path = data + '/backup.txt'
+    f = open(path, 'w', encoding='utf8')
+    contents = mmo.export()
+    f.write(contents)
+    f.close()
+
+    log('created backup')
+
+
 
 def main():
     # fileName = input('Enter path to post: ')
@@ -176,10 +221,15 @@ def main():
     f.close()
     mmo.getLeaderboards(post)
     print('load successful')
+    log('load successful')
 
     while True:
         action = input('\n1 = add/update run\n2 = delete run\n3 = export\n\n4 = view leaderboard\n5 = exit\n')
         runAction(action)
 
+try:
+    main()
+except Exception as error:
+    backup()
 
-main()
+    log('error: ' + str(error))
